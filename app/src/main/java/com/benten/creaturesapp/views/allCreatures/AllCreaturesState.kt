936a26e35811c6acb0b550d5baf2b00e5a6861d7
Tornike kachkachishvili <tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.ge>
2022-09-07 17:00:31 +0400

    added some ccode

---
+++ app/src/main/java/com/benten/creaturesapp/views/allCreatures/AllCreaturesState.kt
@@ -0,0 +1,14 @@
+package com.benten.creaturesapp.views.allCreatures
+
+import com.benten.creaturesapp.model.Creature
+
+sealed class AllCreaturesState {
+    data class Success(val data: List<Creature>) : AllCreaturesState()
+    data class Error(val error: Throwable)
+}
+
+enum class FilterRange(val range: IntRange) {
+    HIT_POINTS_0_50(0..50),
+    HIT_POINTS_50_100(50..100),
+    HIT_POINTS_100_150(100..150)
+}
