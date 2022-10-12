e2e8394c5db7a6d749471b655ec22120b18f6d17
Tornike kachkachishvili <tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.gegit config --global user.email tkachkachishvili@tbcbank.com.ge>
2022-10-12 19:19:31 +0400

    added detekt dependencies

--- app/src/main/java/com/benten/creaturesapp/MainActivity.kt
+++ app/src/main/java/com/benten/creaturesapp/MainActivity.kt
@@ -18,6 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
+
 }
